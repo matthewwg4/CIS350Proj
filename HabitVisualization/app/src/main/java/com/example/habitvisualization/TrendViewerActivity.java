3c51226c0081b76d708439fe04967f0e6ac18cb5
Tea Tran <tatran@seas.upenn.edu>
2019-03-21 00:05:29 -0400

    Finished binary graph

--- HabitVisualization/app/src/main/java/com/example/habitvisualization/TrendViewerActivity.java
+++ HabitVisualization/app/src/main/java/com/example/habitvisualization/TrendViewerActivity.java
@@ -8,8 +8,10 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
 import com.jjoe64.graphview.ValueDependentColor;
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
@@ -91,7 +93,36 @@ public class TrendViewerActivity extends AppCompatActivity {
         calendar = Calendar.getInstance();
 
         dataStorage.addNewHabitTracker("B", new BinaryHabitTracker());
+        HabitTracker habitTrackerB = dataStorage.getHabitTracker("B");
+        tryDate = calendar.getTime();
+        habitTrackerB.putDateInfo(tryDate, false, 10, 3);
+//1
 
+        calendar.add(Calendar.DATE, -1);
+        tryDate = calendar.getTime();
+        habitTrackerB.putDateInfo(tryDate, true, 3, 5);
+//2
+
+        calendar.add(Calendar.DATE, -1);
+        tryDate = calendar.getTime();
+        habitTrackerB.putDateInfo(tryDate, false, 300, 8);
+//3
+
+        calendar.add(Calendar.DATE, -1);
+        tryDate = calendar.getTime();
+        habitTrackerB.putDateInfo(tryDate, false, 30, 4);
+//4
+        calendar.add(Calendar.DATE, -1);
+        tryDate = calendar.getTime();
+        habitTrackerB.putDateInfo(tryDate, true, 30, 4);
+//5
+
+        calendar.add(Calendar.DATE, -1);
+        tryDate = calendar.getTime();
+        habitTrackerB.putDateInfo(tryDate, true, 30, 4);
+        //6
+
+        calendar = Calendar.getInstance();
 
         //testing purpose above------------------------------------
 
@@ -102,9 +133,6 @@ public class TrendViewerActivity extends AppCompatActivity {
         Date newestDateOnAxis = defaultLatestDate;
 
         changeGraph("A"); //testing purpose
-//        String yAxisLeftTitle = getResources().getString(R.string.happiness_unit);
-//        setYAxisLeft(yAxisLeftTitle);
-//        setXAxis(newestDateOnAxis);
     }
 
     // -------------------------------------------------------------------
@@ -112,7 +140,17 @@ public class TrendViewerActivity extends AppCompatActivity {
     // -------------------------------------------------------------------
 
     //The right axis corresponds to the non-happiness data input by the user
-    private void setYAxisRight(float minY, float maxY) {
+    private void setYAxisRight(List<DateInfo> dateInfos) {
+        double minY = Double.POSITIVE_INFINITY;
+        double maxY = Double.NEGATIVE_INFINITY;
+        for (int i = 0; i < dateInfos.size(); i++) {
+            double curUnitVal = dateInfos.get(i).getUnitValue();
+            if (dateInfos.get(i).getUnitValue() < minY) minY = curUnitVal;
+            if (dateInfos.get(i).getUnitValue() > maxY) maxY = curUnitVal;
+        }
+
+        if (minY > 0) minY = 0;
+        maxY += maxY / 5f;
         graph.getSecondScale().setMinY(minY);
         graph.getSecondScale().setMaxY(maxY);
     }
@@ -128,7 +166,6 @@ public class TrendViewerActivity extends AppCompatActivity {
     private void setXAxis(Date newestDate) {
         calendar.setTime(newestDate);
         calendar.add(Calendar.DATE, -numDateShow + 1);
-        Date oldestDate = calendar.getTime();
         calendar = Calendar.getInstance(); //reset the date
 
         graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter
@@ -140,23 +177,54 @@ public class TrendViewerActivity extends AppCompatActivity {
         graph.getGridLabelRenderer().setHumanRounding(false);
     }
 
-    // Line graph corresponds to level of happiness
-    private void setBarGraphHappiness(DataPoint[] dataPoints) {
+    // Bar graph corresponds to level of happiness for binary tracker on days that
+    // an activity is not done
+    private void setBarGraphHappinessBinaryNegative(String habitName, DataPoint[] dataPoints) {
         BarGraphSeries<DataPoint> series = new BarGraphSeries<>(dataPoints);
         graph.addSeries(series);
-        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
-            @Override
-            public int get(DataPoint data) {
-                return ContextCompat.getColor(getBaseContext(), R.color.colorBarGraph);
-            }
-        });
+        int barGraphColor = ContextCompat.getColor(getBaseContext(), R.color.colorBarGraphNegative);
+
         series.setSpacing(50);
+        series.setColor(barGraphColor);
+
+        String seriesTitle = getResources().getString(R.string.happiness_unit_negative) + " "
+                + habitName;
+        series.setTitle(seriesTitle);
+    }
+
+    // Bar graph corresponds to level of happiness for binary tracker on days that
+    // an activity is done
+    private void setBarGraphHappinessBinaryPositive(String habitName, DataPoint[] dataPoints) {
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(dataPoints);
+        graph.addSeries(series);
+        int barGraphColor = ContextCompat.getColor(getBaseContext(), R.color.colorBarGraphPositive);
+
+        series.setSpacing(50);
+        series.setColor(barGraphColor);
+
+        String seriesTitle = getResources().getString(R.string.happiness_unit_positive) + " "
+                + habitName;
+        series.setTitle(seriesTitle);
+    }
+
+    // Bar graph corresponds to level of happiness for numerical tracker
+    private void setBarGraphHappinessNumerical(DataPoint[] dataPoints) {
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(dataPoints);
+        graph.addSeries(series);
+        int barGraphColor = ContextCompat.getColor(getBaseContext(), R.color.colorBarGraph);
+
+        series.setSpacing(50);
+        series.setColor(barGraphColor);
+
+        String seriesTitle = getResources().getString(R.string.happiness_unit);
+        series.setTitle(seriesTitle);
     }
 
     // Bar graph corresponds to the data that is not the level of happiness
-    private void setLineGraphUnitValue(DataPoint[] dataPoints) {
+    private void setLineGraphUnitValue(DataPoint[] dataPoints, String seriesTitle) {
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints);
         graph.getSecondScale().addSeries(series);
+        series.setTitle(seriesTitle);
     }
 
     private void initRecyclerView() {
@@ -220,8 +288,8 @@ public class TrendViewerActivity extends AppCompatActivity {
         return dataPoints;
     }
 
-    //This is to be input into the line graph
-    private DataPoint[] getDataPointsHappiness(Date latestChosenDate, List<DateInfo> dateInfos) {
+    private DataPoint[] getDataPointsHappinessBinaryPositive(Date latestChosenDate,
+                                                             List<DateInfo> dateInfos) {
         DataPoint[] dataPoints = new DataPoint[numDateShow];
         if (dateInfos == null) {
             Log.d(TAG, "getDataPointsHappiness: invalid input");
@@ -238,13 +306,110 @@ public class TrendViewerActivity extends AppCompatActivity {
                 // seven days prior to this day
                 for (int indexDataPoints = 0; indexDataPoints < numDateShow; indexDataPoints++) {
                     calendar.add(Calendar.DATE, -indexDataPoints);
-                   Date curDateToShow = ymdFormat.parse(ymdFormat.format(calendar.getTime()).toString());
+                    Date curDateToShow = ymdFormat.parse(ymdFormat.format(calendar.getTime()).toString());
                     int happiness = 0;
 
                     // Check if there is data on the date of curDateToShow
                     int indexDateInfos = 0;
                     while (indexDateInfos < dateInfos.size()) {
-                        Date curDateInDateInfos = dateInfos.get(indexDateInfos).getDate();
+                        DateInfo curDateInfo = dateInfos.get(indexDateInfos);
+                        Date curDateInDateInfos = curDateInfo.getDate();
+                        curDateInDateInfos = ymdFormat.parse(ymdFormat.format(curDateInDateInfos));
+                        if (firstDateEqualsSecondDate(curDateToShow, curDateInDateInfos) &&
+                                curDateInfo.isDone()) {
+                            happiness = dateInfos.get(indexDateInfos).getHappiness();
+                            break;
+                        }
+                        indexDateInfos++;
+                    }
+
+                    dataPoints[numDateShow - indexDataPoints - 1] =
+                            new DataPoint(curDateToShow, happiness);
+                    // Reset calendar to the current time
+                    calendar = Calendar.getInstance();
+                }
+            } catch (ParseException e) {
+                Log.d(TAG, "getDataPointsHappiness: got parse exception");
+            }
+        }
+
+        return dataPoints;
+    }
+
+    private DataPoint[] getDataPointsHappinessBinaryNegative(Date latestChosenDate,
+                                                             List<DateInfo> dateInfos) {
+        DataPoint[] dataPoints = new DataPoint[numDateShow];
+        if (dateInfos == null) {
+            Log.d(TAG, "getDataPointsHappiness: invalid input");
+        } else {
+            try {
+                SimpleDateFormat ymdFormat = new SimpleDateFormat("yy/MM/dd");
+
+                // Find the latest date to be shown and the earliest date to be shown
+                // and eliminate the time (only keep year, month and date)
+                Date latestDateNoTime = ymdFormat.parse(ymdFormat.format(latestChosenDate).toString());
+                calendar = Calendar.getInstance(); // reset calendar
+
+                // Iterate through all of the dates from the latestChosenDate to
+                // seven days prior to this day
+                for (int indexDataPoints = 0; indexDataPoints < numDateShow; indexDataPoints++) {
+                    calendar.add(Calendar.DATE, -indexDataPoints);
+                    Date curDateToShow = ymdFormat.parse(ymdFormat.format(calendar.getTime()).toString());
+                    int happiness = 0;
+
+                    // Check if there is data on the date of curDateToShow
+                    int indexDateInfos = 0;
+                    while (indexDateInfos < dateInfos.size()) {
+                        DateInfo curDateInfo = dateInfos.get(indexDateInfos);
+                        Date curDateInDateInfos = curDateInfo.getDate();
+                        curDateInDateInfos = ymdFormat.parse(ymdFormat.format(curDateInDateInfos));
+                        if (firstDateEqualsSecondDate(curDateToShow, curDateInDateInfos) &&
+                                !curDateInfo.isDone()) {
+                            happiness = dateInfos.get(indexDateInfos).getHappiness();
+                            break;
+                        }
+                        indexDateInfos++;
+                    }
+
+                    dataPoints[numDateShow - indexDataPoints - 1] =
+                            new DataPoint(curDateToShow, happiness);
+                    // Reset calendar to the current time
+                    calendar = Calendar.getInstance();
+                }
+            } catch (ParseException e) {
+                Log.d(TAG, "getDataPointsHappiness: got parse exception");
+            }
+        }
+        return dataPoints;
+    }
+
+    //This is to be input into the line graph
+    private DataPoint[] getDataPointsHappinessNumerical(Date latestChosenDate,
+                                                        List<DateInfo> dateInfos) {
+        DataPoint[] dataPoints = new DataPoint[numDateShow];
+        if (dateInfos == null) {
+            Log.d(TAG, "getDataPointsHappiness: invalid input");
+        } else {
+            try {
+                SimpleDateFormat ymdFormat = new SimpleDateFormat("yy/MM/dd");
+
+                // Find the latest date to be shown and the earliest date to be shown
+                // and eliminate the time (only keep year, month and date)
+                Date latestDateNoTime = ymdFormat.parse(ymdFormat.format(latestChosenDate).toString());
+                calendar = Calendar.getInstance(); // reset calendar
+
+                // Iterate through all of the dates from the latestChosenDate to
+                // seven days prior to this day
+                for (int indexDataPoints = 0; indexDataPoints < numDateShow; indexDataPoints++) {
+                    calendar.add(Calendar.DATE, -indexDataPoints);
+                    Date curDateToShow = ymdFormat.parse(ymdFormat.format(calendar.getTime()).toString());
+                    int happiness = 0;
+
+                    // Check if there is data on the date of curDateToShow
+                    int indexDateInfos = 0;
+                    while (indexDateInfos < dateInfos.size()) {
+                        DateInfo curDateInfo = dateInfos.get(indexDateInfos);
+                        Date curDateInDateInfos = curDateInfo.getDate();
                         curDateInDateInfos = ymdFormat.parse(ymdFormat.format(curDateInDateInfos));
                         if (firstDateEqualsSecondDate(curDateToShow, curDateInDateInfos)) {
                             happiness = dateInfos.get(indexDateInfos).getHappiness();
@@ -298,39 +463,29 @@ public class TrendViewerActivity extends AppCompatActivity {
         }
     }
 
-    private boolean firstDateBeforeSecondDate(Date date1, Date date2) {
-        SimpleDateFormat ymdFormat = new SimpleDateFormat("yy/MM/dd");
-        try {
-            Date date1NoTime = ymdFormat.parse(ymdFormat.format(date1));
-            Date date2NoTime = ymdFormat.parse(ymdFormat.format(date2));
-            if (date1NoTime.before(date2NoTime)) {
-                return true;
-            }
-        } catch (ParseException e) {
-            Log.d(TAG, "firstDateBeforeSecondDate: parse exception");
-            e.printStackTrace();
-        }
-        return false;
+    private void setLegend() {
+        int backgroundColor = ContextCompat.getColor(getBaseContext(), R.color.colorLegendBackground);
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setTextSize(30);
+        graph.getLegendRenderer().setSpacing(20);
+        graph.getLegendRenderer().setBackgroundColor(backgroundColor);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
     }
 
-    private boolean firstDateAfterSecondDate(Date date1, Date date2) {
-        SimpleDateFormat ymdFormat = new SimpleDateFormat("yy/MM/dd");
-        try {
-            Date date1NoTime = ymdFormat.parse(ymdFormat.format(date1));
-            Date date2NoTime = ymdFormat.parse(ymdFormat.format(date2));
-            if (date1NoTime.after(date2NoTime)) {
-                return true;
+    private void setAxisDescription(String unitName, HabitType habitType) {
+        String happiness_unit = getResources().getString(R.string.happiness_unit);
+        TextView textView = findViewById(R.id.axisDescription);
+        String description = "The unit of the left y-axis is " + happiness_unit + ".";
+        if (habitType == habitType.NUMERICAL) {
+            if (unitName == null) {
+                Log.d(TAG, "setAxisDescription: error got null unitName for numerical tracker");
             }
-        } catch (ParseException e) {
-            Log.d(TAG, "firstDateAfterSecondDate: parse exception");
-            e.printStackTrace();
+            description = description + "\nThe unit of the right y-axis is " + unitName + ".";
         }
-        return false;
+        textView.setText(description);
     }
-
-
     // -------------------------------------------------------------------
-    // PUBLIC FUNCTIONS --------------------------------------------------s
+    // PUBLIC FUNCTIONS --------------------------------------------------
     // -------------------------------------------------------------------
 
     public void changeGraph(String habitNameChosen) {
@@ -339,28 +494,47 @@ public class TrendViewerActivity extends AppCompatActivity {
         HabitTracker habitTracker = dataStorage.getHabitTracker(habitNameChosen);
 
         if (habitTracker != null) {
-            graph.removeAllSeries();
             List<DateInfo> dateInfos = habitTracker.getTracking();
+            HabitType habitType = habitTracker.getHabitType();
 
+            graph.removeAllSeries();
+            graph.clearSecondScale();
             Date latestChosenDate = calendar.getTime();
 
-            //Set happiness data (bar graph)
-            DataPoint[] happyDataPoints = getDataPointsHappiness(latestChosenDate, dateInfos);
-            setBarGraphHappiness(happyDataPoints);
-
-            HabitType habitType = habitTracker.getHabitType();
             if (habitType == HabitType.NUMERICAL) {
-                Log.d(TAG, "nume");
-                DataPoint[] unitValueDataPoints = getDataPointsUnitValues(latestChosenDate, dateInfos);
-                setLineGraphUnitValue(unitValueDataPoints);
-                setYAxisRight(0, 500); //TOBE changed
-            } else {
+                // Set happiness data (bar graph)
+                DataPoint[] happyDataPoints =
+                        getDataPointsHappinessNumerical(latestChosenDate, dateInfos);
+                setBarGraphHappinessNumerical(happyDataPoints);
 
+                // Set unit value data (line graph)
+                DataPoint[] unitValueDataPoints = getDataPointsUnitValues(latestChosenDate, dateInfos);
+                setLineGraphUnitValue(unitValueDataPoints, habitTracker.getUnitName());
+                setYAxisRight(dateInfos);
+            } else {
+                //TOBE DELETED
+                Log.d(TAG, "changeGraph: BINARY");
+                for (int i = 0; i < dateInfos.size(); i++) {
+                    Log.d(TAG, "changeGraph: date" + dateInfos.get(i).getDate().toString());
+                    Log.d(TAG, "changeGraph: happiness" + dateInfos.get(i).getHappiness());
+                }
+
+                //Get happiness on days that a certain activity is done
+                DataPoint[] happyDataPointsPos =
+                        getDataPointsHappinessBinaryPositive(latestChosenDate, dateInfos);
+                setBarGraphHappinessBinaryPositive(habitNameChosen, happyDataPointsPos);
+
+                //Get happiness on days that a certain activity is not done
+                DataPoint[] happyDataPointsNeg =
+                        getDataPointsHappinessBinaryNegative(latestChosenDate, dateInfos);
+                setBarGraphHappinessBinaryNegative(habitNameChosen, happyDataPointsNeg);
             }
 
             setGraphTitle(habitNameChosen, latestChosenDate);
             setXAxis(latestChosenDate);
             setYAxisLeft();
+            setLegend();
+            setAxisDescription(habitTracker.getUnitName(), habitType);
         } else {
             Log.d(TAG, "changeGraph: cannot find a habitTracker with the given habit name");
         }
