eb713e2b02ab1d2dfe54188343946abbe7496ccd
Tea Tran <tatran@seas.upenn.edu>
2019-03-24 05:32:05 -0400

    Allow user to see past data on the graph

--- HabitTracker/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/transition/R.java
+++ HabitTracker/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/transition/R.java
@@ -99,23 +99,23 @@ public final class R {
         public static final int notification_main_column = 0x7f09007d;
         public static final int notification_main_column_container = 0x7f09007e;
         public static final int parent_matrix = 0x7f090085;
-        public static final int right_icon = 0x7f090094;
-        public static final int right_side = 0x7f090095;
-        public static final int save_image_matrix = 0x7f090096;
-        public static final int save_non_transition_alpha = 0x7f090097;
-        public static final int save_scale_type = 0x7f090098;
-        public static final int tag_transition_group = 0x7f0900c9;
-        public static final int tag_unhandled_key_event_manager = 0x7f0900ca;
-        public static final int tag_unhandled_key_listeners = 0x7f0900cb;
-        public static final int text = 0x7f0900cc;
-        public static final int text2 = 0x7f0900cd;
-        public static final int time = 0x7f0900d6;
-        public static final int title = 0x7f0900d7;
-        public static final int transition_current_scene = 0x7f0900dd;
-        public static final int transition_layout_save = 0x7f0900de;
-        public static final int transition_position = 0x7f0900df;
-        public static final int transition_scene_layoutid_cache = 0x7f0900e0;
-        public static final int transition_transform = 0x7f0900e1;
+        public static final int right_icon = 0x7f090097;
+        public static final int right_side = 0x7f090098;
+        public static final int save_image_matrix = 0x7f090099;
+        public static final int save_non_transition_alpha = 0x7f09009a;
+        public static final int save_scale_type = 0x7f09009b;
+        public static final int tag_transition_group = 0x7f0900cc;
+        public static final int tag_unhandled_key_event_manager = 0x7f0900cd;
+        public static final int tag_unhandled_key_listeners = 0x7f0900ce;
+        public static final int text = 0x7f0900cf;
+        public static final int text2 = 0x7f0900d0;
+        public static final int time = 0x7f0900d9;
+        public static final int title = 0x7f0900da;
+        public static final int transition_current_scene = 0x7f0900e1;
+        public static final int transition_layout_save = 0x7f0900e2;
+        public static final int transition_position = 0x7f0900e3;
+        public static final int transition_scene_layoutid_cache = 0x7f0900e4;
+        public static final int transition_transform = 0x7f0900e5;
     }
     public static final class integer {
         private integer() {}
@@ -125,12 +125,12 @@ public final class R {
     public static final class layout {
         private layout() {}
 
-        public static final int notification_action = 0x7f0c003e;
-        public static final int notification_action_tombstone = 0x7f0c003f;
-        public static final int notification_template_custom_big = 0x7f0c0046;
-        public static final int notification_template_icon_group = 0x7f0c0047;
-        public static final int notification_template_part_chronometer = 0x7f0c004b;
-        public static final int notification_template_part_time = 0x7f0c004c;
+        public static final int notification_action = 0x7f0c0040;
+        public static final int notification_action_tombstone = 0x7f0c0041;
+        public static final int notification_template_custom_big = 0x7f0c0048;
+        public static final int notification_template_icon_group = 0x7f0c0049;
+        public static final int notification_template_part_chronometer = 0x7f0c004d;
+        public static final int notification_template_part_time = 0x7f0c004e;
     }
     public static final class string {
         private string() {}
