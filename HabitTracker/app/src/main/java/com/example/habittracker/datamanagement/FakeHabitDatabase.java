859223f81bb788e76026df2bf82e8627629b8863
Tea Tran <tatran@seas.upenn.edu>
2019-03-24 04:30:00 -0400

    added garbage data

--- HabitTracker/app/src/main/java/com/example/habittracker/datamanagement/FakeHabitDatabase.java
+++ HabitTracker/app/src/main/java/com/example/habittracker/datamanagement/FakeHabitDatabase.java
@@ -1,5 +1,6 @@
 package com.example.habittracker.datamanagement;
 
+import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -15,11 +16,29 @@ public class FakeHabitDatabase {
         tags.add("relaxation");
         BinaryHabitTracker bht = new BinaryHabitTracker("Meditation", tags, true);
         habits.put("Meditation1", bht);
+        Calendar calendar = Calendar.getInstance();
+        bht.putDateInfo(calendar.getTime(), true, 10);
+        calendar.add(Calendar.DATE , -1);
+        bht.putDateInfo(calendar.getTime(), false, 3);
+        calendar.add(Calendar.DATE , -1);
+        bht.putDateInfo(calendar.getTime(), false, 4);
+        calendar.add(Calendar.DATE , -1);
+        bht.putDateInfo(calendar.getTime(), true, 8);
+        calendar.add(Calendar.DATE , -1);
+        bht.putDateInfo(calendar.getTime(), true, 9);
+calendar = Calendar.getInstance();
 
         Set<String> tags2 = new TreeSet<>();
         tags.add("sleep");
         NumericalHabitTracker nht = new NumericalHabitTracker("Hours Slept", tags2, true, "hours");
         habits.put("Sleep1", nht);
+nht.putDateInfo(calendar.getTime(), 10, 10);
+        calendar.add(Calendar.DATE , -1);
+nht.putDateInfo(calendar.getTime(), 20, 3);
+        calendar.add(Calendar.DATE , -1);
+        nht.putDateInfo(calendar.getTime(), 38, 10);
+        calendar.add(Calendar.DATE , -1);
+        nht.putDateInfo(calendar.getTime(), 3, 1);
     }
 
     public HabitTracker getHabitTracker(String habitName) {
