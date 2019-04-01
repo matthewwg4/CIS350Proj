325a59c44e8524122fe17287220adfd892e30217
Tea Tran <tatran@seas.upenn.edu>
2019-04-01 01:25:21 -0400

    added view user info page basic with username, password and buttons to update user's field. not habits yet

--- CIS350MS2/index.js
+++ CIS350MS2/index.js
@@ -20,18 +20,18 @@ app.use('/public', express.static('public'));
 
 // route for creating a new user
 // this is the action of the "create new person" form
-	 
+
 app.use('/addUser', (req, res) => {
 	// construct the Person from the form data which is in the request body
 	var newUser = new User ({
 		userName: req.body.username,
 		password: req.body.password,
-	    });
+	});
 
 	// save the person to the database
 	newUser.save( (err) => { 
 		if (err) {
-		    
+
 //Jialin's original code
 		    // res.type('html').status(200);
 		    // res.write('uh oh: ' + err);
@@ -39,26 +39,37 @@ app.use('/addUser', (req, res) => {
 		    // res.end();
 
 			// Tea's code:
-			  res.render('addUserFailed');
+			res.render('addUserFailed');
 		}
 		else {
 		    // display the "successfull created" page using EJS
 		    res.render('created', {user : newUser});
 		}
-	    } ); 	
+	} ); 	
+});
+
+// Displays the user with username ':name'
+app.use('/user/:name', (req, res) => {
+	// console.log("reached");
+	// res.send(req.params.name);
+	User.findOne({userName: req.params.name}, (err, user) => {
+		if (err) {res.type('html').status(500);res.send('Error: ' + err);}
+		else {
+			res.render('viewUserInfo', {user : user})
+		}
+	});
 });
 
 app.use('/view', (req, res) => 
 	User.find( (err, allUsers) => {
 		if (err) {res.type('html').status(500);res.send('Error: ' + err); 
-		}
-		else if (allUsers.length == 0) {res.type('html').status(200);res.send('There are no users');
-		}
-		else {res.render('viewAll', { user: allUsers 
-		})};
-	}));
-	
-	
+	}
+	else if (allUsers.length == 0) {res.type('html').status(200);res.send('There are no users');
+}
+else {res.render('viewAll', { user: allUsers})};
+}
+));
+
 
 app.use( /*default*/ (req, res) => {  res.status(404).send('Not found!');});
 
@@ -75,7 +86,7 @@ app.use( /*default*/ (req, res) => {  res.status(404).send('Not found!');});
 	    // // if there's a name in the query parameter, use it here
 	    // queryObject = { "name" : req.query.name };
 	// }
-    
+
 	// Person.find( queryObject, (err, persons) => {
 		// console.log(persons);
 		// if (err) {
@@ -107,11 +118,11 @@ app.use( /*default*/ (req, res) => {  res.status(404).send('Not found!');});
 
 
 
-/*************************************************/
+    /*************************************************/
 
 
 
 
-app.listen(3000,  () => {
-	console.log('Listening on port 3000');
+    app.listen(3000,  () => {
+    	console.log('Listening on port 3000');
     });
